6aa05e092acdc52e6624f280567b0367bbaac0cf
ZippyRainbow <paul@interacct.com.au>
2025-07-02 09:48:27 +1000

    Update and remove PluginRegistry

--- android/src/main/java/me/andisemler/nfc_in_flutter/NfcInFlutterPlugin.java
+++ android/src/main/java/me/andisemler/nfc_in_flutter/NfcInFlutterPlugin.java
@@ -2,29 +2,15 @@ package me.andisemler.nfc_in_flutter;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
-import io.flutter.plugin.common.PluginRegistry;
-import io.flutter.plugin.common.PluginRegistry.Registrar;
 import io.flutter.embedding.engine.plugins.FlutterPlugin;
 import io.flutter.embedding.engine.plugins.activity.ActivityAware;
 import io.flutter.embedding.engine.plugins.activity.ActivityPluginBinding;
+import io.flutter.plugin.common.MethodChannel;
 
-/**
- * NfcInFlutterPlugin
- */
-public class NfcInFlutterPlugin implements FlutterPlugin,
-        ActivityAware{
-
+public class NfcInFlutterPlugin implements FlutterPlugin, ActivityAware {
     private @Nullable FlutterPluginBinding flutterPluginBinding;
-    private static @Nullable MethodCallHandlerImpl methodCallHandler;
-
-    /**
-     * Plugin registration.
-     */
-    @SuppressWarnings("deprecation")
-    public static void registerWith(Registrar registrar) {
-        methodCallHandler = new MethodCallHandlerImpl(registrar.activity(), registrar.messenger());
-        registrar.addNewIntentListener(methodCallHandler);
-    }
+    private @Nullable MethodCallHandlerImpl methodCallHandler;
+    private @Nullable MethodChannel channel;
 
     public NfcInFlutterPlugin() {
     }
@@ -32,16 +18,37 @@ public class NfcInFlutterPlugin implements FlutterPlugin,
     @Override
     public void onAttachedToEngine(@NonNull FlutterPluginBinding binding) {
         this.flutterPluginBinding = binding;
+        // Initialize method channel
+        channel = new MethodChannel(binding.getBinaryMessenger(), "nfc_in_flutter");
     }
 
     @Override
     public void onDetachedFromEngine(@NonNull FlutterPluginBinding binding) {
+        // Clean up engine references
+        if (channel != null) {
+            channel.setMethodCallHandler(null);
+            channel = null;
+        }
         this.flutterPluginBinding = null;
     }
 
     @Override
     public void onAttachedToActivity(@NonNull ActivityPluginBinding binding) {
-        methodCallHandler = new MethodCallHandlerImpl(binding.getActivity(), flutterPluginBinding.getBinaryMessenger());
+        if (flutterPluginBinding == null) {
+            return;
+        }
+
+        methodCallHandler = new MethodCallHandlerImpl(
+                binding.getActivity(),
+                flutterPluginBinding.getBinaryMessenger()
+        );
+
+        // Set up method channel handler
+        if (channel != null) {
+            channel.setMethodCallHandler(methodCallHandler);
+        }
+
+        // Register for new intents
         binding.addOnNewIntentListener(methodCallHandler);
     }
 
@@ -57,11 +64,14 @@ public class NfcInFlutterPlugin implements FlutterPlugin,
 
     @Override
     public void onDetachedFromActivity() {
-        // Could be on too low of an SDK to have started listening originally.
         if (methodCallHandler != null) {
             methodCallHandler.stopListening();
             methodCallHandler = null;
-          }
-    }
+        }
 
+        // Clear method channel handler when detached from activity
+        if (channel != null) {
+            channel.setMethodCallHandler(null);
+        }
+    }
 }
